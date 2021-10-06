1512bc4a3a6e0081d31416dc8fb1d671a3f9793b
dmax66 <46436425+dmax66@users.noreply.github.com>
2021-10-06 18:40:22 +0200

    Delete init_db.sql

--- db/init_db.sql
+++
@@ -1,9 +0,0 @@
-DROP USER game_engine_user;
-
-CREATE USER game_engine_user@localhost IDENTIFIED BY '3etomerG';
-
-CREATE DATABASE game_engine;
-
-GRANT CREATE, DROP, ALTER, DELETE, INSERT, SELECT, UPDATE, INDEX, REFERENCES ON game_engine.* TO game_engine_user@localhost;
-
-FLUSH PRIVILEGES;
