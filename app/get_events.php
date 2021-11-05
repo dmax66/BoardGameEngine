9af4654a16a28cf8fec1f3b2eeb9a0500eb6db18
dmax66 <46436425+dmax66@users.noreply.github.com>
2021-11-05 19:58:05 +0000

    Add files via upload

---
+++ app/get_events.php
@@ -0,0 +1,50 @@
+<?php
+// Load from the database a list of scenarios and return the values
+
+include 'db_params.php';
+include 'log.php';
+
+if ($loglevel > 0) fprintf ($logfile, "Entering %s\n", __FILE__);
+
+// Create connection
+if ($loglevel > 0) fwrite ($logfile, "Opening connection...");
+
+$conn = new mysqli($servername, $username, $password);
+
+if ($conn->connect_error)
+{
+	if ($loglevel > 0)fwrite ($logfile, "failed\n");
+	die();
+}
+else if ($loglevel > 0)	fwrite ($logfile, "success\n");
+
+
+$sql = "USE game_engine";
+$result = mysqli_query($conn, $sql);
+
+
+$sql = "SELECT * FROM Events WHERE Turn = " . $_GET['turn'];
+if ($loglevel > 1) fprintf ($logfile, "SQL=%s\n", $sql);
+
+
+if ($loglevel > 0) fwrite($logfile, "Querying result...");
+
+$result = mysqli_query($conn, $sql);
+
+if ($loglevel > 0) fwrite($logfile, "done\n");
+
+
+if (mysqli_num_rows($result) > 0) 
+{
+	$outp = $result->fetch_all(MYSQLI_ASSOC);
+	echo json_encode($outp);
+	if ($loglevel > 0) fwrite ($logfile, "Result set OK\n");
+} 
+else
+{
+	if ($loglevel) fwrite ($logfile, "No rows\n");
+	echo "ERROR: no rows";
+}
+
+mysqli_close($conn);
+fclose($logfile);
