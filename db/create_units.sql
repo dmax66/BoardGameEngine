7c850a696d389be10c697511a58906e0f5b3a1cc
dmax66 <46436425+dmax66@users.noreply.github.com>
2021-11-11 14:28:08 +0100

    Fixed error in column name

--- db/create_units.sql
+++ db/create_units.sql
@@ -1,19 +1,19 @@
 CREATE VIEW Units AS 
 SELECT 
-	SD.id AS id,
+    SD.id AS id,
     SD.name AS name,
     SD.commander AS commander,
     SD.nation AS nation,
     SD.size AS size,
     SD.type AS type,
-    DD.gameId AS gameId,
-    DD.unitId AS unitId,
+    DD.game_ID AS gameId,
+    DD.id AS unitId,
     DD.commandedBy AS commandedBy,
     DD.strength AS strength,
     N.playerId AS playerId 
 FROM
     Units_Static_Data SD, Units_Dynamic_Data DD, Nations_Static_Data N
 WHERE 
-	SD.id = DD.unitId 
+    SD.id = DD.id 
     AND 
     SD.nation = N.id;
--- db/create_units_dynamic_data.sql
+++ db/create_units_dynamic_data.sql
@@ -7,6 +7,6 @@ CREATE TABLE `Units_Dynamic_Data` (
   `exitTurn` int DEFAULT NULL,
   KEY `fk_Game_idx` (`game_ID`),
   KEY `fk_Units_Static_Data_idx` (`id`),
-  CONSTRAINT `fk_Game` FOREIGN KEY (`game_ID`) REFERENCES `Games` (`ID`),
+  CONSTRAINT `fk_Game` FOREIGN KEY (`game_ID`) REFERENCES `Games` (`gameId`),
   CONSTRAINT `fk_Units_Static_Data` FOREIGN KEY (`id`) REFERENCES `Units_Static_Data` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
