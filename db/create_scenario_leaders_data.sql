af2bbab01f717a57f728d42da05d7f886d9681ae
dmax66 <46436425+dmax66@users.noreply.github.com>
2021-11-11 14:26:44 +0100

    Fixed error in Foreign Key referenced Table

--- db/create_scenario_leaders_data.sql
+++ db/create_scenario_leaders_data.sql
@@ -10,6 +10,6 @@ CREATE TABLE `Scenario_Leaders_Data` (
   KEY `fk_Scenario_Leaders_Data_Leaders_Static_Data1_idx` (`id`),
   KEY `fk_2_idx` (`parentId`),
   CONSTRAINT `fk1` FOREIGN KEY (`id`) REFERENCES `Leaders_Static_Data` (`id`),
-  CONSTRAINT `fk2` FOREIGN KEY (`parentId`) REFERENCES `Scenario_Leaders_Data` (`id`),
+  CONSTRAINT `fk2` FOREIGN KEY (`parentId`) REFERENCES `Leaders_Static_Data` (`id`),
   CONSTRAINT `fk3` FOREIGN KEY (`Scenario_ID`) REFERENCES `Scenarios` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
