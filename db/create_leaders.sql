158ee6699684b80b248c2398a716182c0185cbfc
dmax66 <46436425+dmax66@users.noreply.github.com>
2021-11-15 22:57:22 +0100

    Add files via upload

--- db/create_leaders.sql
+++ db/create_leaders.sql
@@ -1,4 +1,24 @@
-CREATE VIEW `Leaders` AS 
-SELECT`SD`.`id` AS `id`,`SD`.`name` AS `name`,`SD`.`nation` AS `nation`,`SD`.`player` AS `player`,`SD`.`armyName` AS `armyName`,`SD`.`unitName` AS `unitName`,`SD`.`type` AS `type`,`SD`.`initiative` AS `initiative`,`SD`.`hasBonus` AS `hasBonus`,`SD`.`commandCapacity` AS `commandCapacity`,`SD`.`subordinationValue` AS `subordinationValue`,`DD`.`gameId` AS `gameId`,`DD`.`leaderId` AS `leaderId`,`DD`.`X` AS `X`,`DD`.`Y` AS `Y`,`DD`.`orientation` AS `orientation`,`DD`.`mode` AS `mode`,`DD`.`parentId` AS `parentId`,`DD`.`zOrder` AS `zOrder` 
-FROM `Leaders_Static_Data` `SD`, `Leaders_Dynamic_Data` `DD`
-WHERE (`SD`.`id` = `DD`.`leaderId`);
+CREATE VIEW Leaders AS 
+SELECT
+  DD.gameId,
+  SD.playerId,
+  DD.leaderId,
+  SD.nationId,
+  SD.name,
+  SD.armyName,
+  SD.unitName,
+  SD.type,
+  SD.initiative,
+  SD.hasBonus,
+  SD.commandCapacity,
+  SD.subordinationValue,
+  DD.X,
+  DD.Y,
+  DD.orientation,
+  DD.mode,
+  DD.parentId,
+  DD.zOrder
+FROM 
+  Leaders_Static_Data SD, Leaders_Dynamic_Data DD
+WHERE 
+  SD.leaderId = DD.leaderId;
