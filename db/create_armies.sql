eb42830923449be5e43f281c2c45906e3e6e1ac0
dmax66 <46436425+dmax66@users.noreply.github.com>
2021-12-21 16:37:41 +0100

    Added the active Supply Source to the view

--- db/create_armies.sql
+++ db/create_armies.sql
@@ -8,7 +8,10 @@ SELECT
         DD.COP_x,
         DD.COP_y,
         DD.COP_isActive,
-        DD.COP_turnToReactivate
+        DD.COP_turnToReactivate,
+        DD.SS_x,
+        DD.SS_y,
+        DD.SS_isActive
 FROM 
         Armies_Static_Data SD, Armies_Dynamic_Data DD 
 WHERE
